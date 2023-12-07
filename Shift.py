6ec814370fee66439e5f32c50086c690f708a282
eshwar-u <eshwar.umarengan@gmail.com>
2023-12-07 14:12:56 -0500

    created shift.py

---
+++ Shift.py
@@ -0,0 +1,29 @@
+'''
+This file stores the object for one shift.
+Author: Eshwar Umarengan
+Date: 12/7/2023
+'''
+
+class Shift:
+    def __init__(self, startTime, endTime, day):
+        self.startTime = startTime
+        self.endTime = endTime
+        self.day = day
+
+    def getStartTime(self):
+        return self.startTime
+
+    def getEndTime(self):
+        return self.endTime
+
+    def getDay(self):
+        return self.day
+
+    def setStartTime(self, startTime):
+        self.startTime = startTime
+
+    def setEndTime(self, endTime):
+        self.endTime = endTime
+
+    def setDay(self, day):
+        self.day = day
